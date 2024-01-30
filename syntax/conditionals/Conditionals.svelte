8473ef36dbb7acb2be304f8fb4bd8f658d74bcd7
Pablo Sirera <psirera4@gmail.com>
2024-01-30 23:04:39 +0100

    add syntax examples

---
+++ syntax/conditionals/Conditionals.svelte
@@ -0,0 +1,25 @@
+<script>
+  const TRAFFIC_LIGHTS = ['red', 'orange', 'green'];
+  let lightIndex = 0;
+
+  $: light = TRAFFIC_LIGHTS[lightIndex];
+
+  function nextLight() {
+    lightIndex = lightIndex + 1;
+  }
+</script>
+
+<button on:click={nextLight}>Next light</button>
+<p>Light is: {light}</p>
+<p>
+  You must
+  {#if light === 'red'}
+    <span>STOP</span>
+  {:else if light === 'orange'}
+    <span>SLOW DOWN</span>
+  {:else if light === 'green'}
+    <span>GO</span>
+  {:else}
+    <span>NOTHING</span>
+  {/if}
+</p>
---
+++ syntax/conditionals/Conditionals.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, computed } from "vue"
+
+const TRAFFIC_LIGHTS = ["red", "orange", "green"]
+const lightIndex = ref(0)
+
+const light = computed(() => TRAFFIC_LIGHTS[lightIndex.value])
+
+const nextLight = () => {
+  lightIndex.value = lightIndex.value + 1
+}
+</script>
+
+<template>
+  <button @click="nextLight">Next light</button>
+  <p>Light is: {{ light }}</p>
+  <p>
+    You must
+    <span v-if="light === 'red'">STOP</span>
+    <span v-else-if="light === 'orange'">SLOW DOWN</span>
+    <span v-else-if="light === 'green'">GO</span>
+    <span v-else>NOTHING</span>
+  </p>
+</template>
